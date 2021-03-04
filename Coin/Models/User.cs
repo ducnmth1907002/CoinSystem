59167e8b3299375642640471a94712d1b8e0e7e2
ducnmth1907002 <ducnmth1907002@fpt.edu.vn>
2021-03-04 21:57:19 +0700

    register/login

--- Coin/Models/User.cs
+++ Coin/Models/User.cs
@@ -1,22 +1,40 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace Coin.Models
 {
-    public class User
+    public class User : IdentityUser
     {
-        public int Id { get; set; }
+        //[Key]
+        //public int UserId { get; set; }
         public string Name { get; set; }
         public string Phone { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        //[Required]
+        //[DataType(DataType.EmailAddress)]
+        //public string Email { get; set; }
+        //[Required]
+        //[StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        //[DataType(DataType.Password)]
+        //[Display(Name = "Password")]
+        //public string Password { get; set; }
+
+        //[DataType(DataType.Password)]
+        //[Display(Name = "Confirm password")]
+        //[Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        //public string ConfirmPassword { get; set; }
         public UserRole Role { get; set; }
         public UserStatus Status { get; set; }
         public ICollection<Subscribe> Subscribes { get; set; }
         public ICollection<News> News { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
     }
+
 
     public enum UserRole
     {
@@ -27,4 +45,5 @@ namespace Coin.Models
     {
         Active, Deactive
     }
+
 }
